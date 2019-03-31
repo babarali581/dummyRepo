081c11f90c0e2880ad055d6315bf688789aeb811
babarali581 <cloudnosys.babar@gmail.com>
2019-03-31 16:20:07 +0500

    bab2

--- dummy.js
+++ dummy.js
@@ -1,11 +1,9 @@
 var http = require('http')
 
 // create a server object:
-http
-  .createServer(function (req, res) {
-    res.write('Hello World bab!') // write a response to the client
-
-    res.end() // end the response
+http.createServer(function (req, res) {
+  res.write('Hello World bab!2') // write a response to the client
+  res.end() // end the response
   })
   
   .listen(8080) // the server object listens on port 8080
